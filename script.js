0ffb46395dcc48c7651d1e167908059cfb30b0a5
Abhishek97Ojha <abhishek97ojha@gmail.com>
2023-01-16 02:03:33 +0530

    Responsive.

--- script.js
+++ script.js
@@ -5,16 +5,30 @@ let middle = document.getElementById("middle");
 let sortData = countries_data.sort((a, b) => {
     return b.population - a.population;
 });
+// console.log(sortData);
 let countryName = [];
 let populationOf = [];
 let totalPopulation = 0;
+let lang = [];
 sortData.map((ele) => {
     countryName.push(ele.name);
     populationOf.push(ele.population);
     totalPopulation += ele.population;
+    lang.push(ele.languages);
 });
-
-
+// console.log(lang);
+let flatLang = lang.flat();
+// console.log(flatLang);
+let langCount = {};
+flatLang.map((ele) => {
+    langCount[ele] = (langCount[ele] || 0) + 1;
+})
+// console.log(langCount);
+let langName = Object.entries(langCount);
+// console.log(langName);
+let langSort = langName.sort((a, b) => {
+    return b[1] - a[1];
+});
 function population() {
     console.log("population");
     middle.innerText = "10 Most populated countries in the World";
@@ -40,10 +54,22 @@ function population() {
              <div class="total" >${populationOf[i]}</div>
          </div>`
     }
-
 }
 function language() {
     console.log("language");
     middle.innerText = "10 Most Spoken languages in the World";
-    detail.innerHTML = ""
+    detail.innerHTML = "";
+    for (i = 0; i < 10; i++) {
+        let langArr = langSort[i];
+        console.log(langArr);
+        let percent = ((langArr[1]/ langSort.length) * 100).toFixed(2);
+        detail.innerHTML +=
+            `<div id="data">
+             <div class="name">${langArr[0]}</div>
+             <div class="bar">
+                 <div class="color" id="color" style="width:${percent}% ;"></div>
+             </div>
+             <div class="total" >${langArr[1]}</div>
+         </div>`
+    }
 }
